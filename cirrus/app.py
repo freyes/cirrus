0613535b7089c734e147d9423564214d369dac67
Jorge Niedbalski <jorge@nimbic.com>
2014-02-12 11:32:31 -0300

    [app] connect to ssh with right-click

--- cirrus/app.py
+++ cirrus/app.py
@@ -11,6 +11,7 @@ from cirrus.ec2 import ListInstancesThread
 gi.require_version("Gtk", "3.0")
 
 from gi.repository import Gtk
+from gi.repository import GObject
 from gi.repository import Vte
 from gi.repository import Gdk
 from gi.repository import GdkPixbuf
@@ -56,32 +57,75 @@ def instance_age(instance):
                                              "%H:%M %m/%d/%Y"))
 
 
+class TabLabel(Gtk.Box):
+    __gsignals__ = {
+        "close-clicked": (GObject.SIGNAL_RUN_FIRST,
+                          GObject.TYPE_NONE, ()),
+    }
+
+    def __init__(self, label_text):
+        Gtk.Box.__init__(self)
+        self.set_orientation(Gtk.Orientation.HORIZONTAL)
+        self.set_spacing(5)
+
+        icon = Gtk.Image()
+        icon.set_from_file(_get_image_path('terminal-32x32.png'))
+        self.pack_start(icon, False, False, 0)
+
+        label = Gtk.Label(label_text)
+        self.pack_start(label, True, True, 0)
+
+        button = Gtk.Button()
+        button.set_relief(Gtk.ReliefStyle.NONE)
+        button.set_focus_on_click(False)
+        button.add(Gtk.Image.new_from_stock(Gtk.STOCK_CLOSE,
+                                            Gtk.IconSize.MENU))
+        button.connect("clicked", self.button_clicked)
+        data = ".button {\n" \
+               "-GtkButton-default-border : 0px;\n" \
+               "-GtkButton-default-outside-border : 0px;\n" \
+               "-GtkButton-inner-border: 0px;\n" \
+               "-GtkWidget-focus-line-width : 0px;\n" \
+               "-GtkWidget-focus-padding : 0px;\n" \
+               "padding: 0px;\n" \
+               "}"
+
+        provider = Gtk.CssProvider()
+        provider.load_from_data(data)
+        button.get_style_context().add_provider(provider, 600)
+        self.pack_start(button, False, False, 0)
+        self.show_all()
+
+    def button_clicked(self, button, data=None):
+        self.emit("close-clicked")
+
+
 class InstanceContextMenu(Gtk.Menu):
     """
     Class for manage the right-click context menu item
     """
     def __init__(self, builder, instance):
         Gtk.Menu.__init__(self)
+
         self.builder = builder
         self.instance = instance
-        self.display_menu()
+        self.show_menu()
 
-    def display_menu(self):
+    def show_menu(self):
         state = self.instance.extra.get("status")
-
         terminal = Gtk.Image()
         terminal.set_from_file(_get_image_path('terminal-32x32.png'))
 
-        ssh_connect = Gtk.ImageMenuItem("Connect via SSH (%s)" % state)
-        ssh_connect.set_image(terminal)
-        ssh_connect.set_always_show_image(True)
+        connect_menu = Gtk.ImageMenuItem("Connect via SSH (%s)" % state)
+        connect_menu.set_image(terminal)
+        connect_menu.set_always_show_image(True)
 
         if state not in ('running', ):
-            ssh_connect.set_sensitive(False)
+            connect_menu.set_sensitive(False)
 
-        ssh_connect.connect("activate", self.on_connect_clicked)
+        connect_menu.connect("activate", self.on_connect_menu_clicked)
 
-        self.append(ssh_connect)
+        self.append(connect_menu)
         self.show_all()
 
     def populate_ip_addrs(self):
@@ -113,37 +157,43 @@ class InstanceContextMenu(Gtk.Menu):
         ipaddr_combo.set_model(ip_store)
         ipaddr_combo.set_active(0)
 
-    def _build_connection_settings(self):
+    def create_connection_settings(self):
         self.view = self.builder.get_object("connection_settings")
-        self.view.connect("delete_event", self.cancel_btn_clicked)
+        self.view.connect("delete_event",
+                          self.on_connection_settings_cancelled)
         self.connect_btn = self.builder.get_object("connect_btn")
         self.cancel_btn = self.builder.get_object("connect_cancel_btn")
-        self.connect_handler = self.connect_btn.connect(
-            "clicked",
-            self.connect_btn_clicked)
-        self.cancel_handler = self.cancel_btn.connect(
-            "clicked",
-            self.cancel_btn_clicked)
 
-    def on_connect_clicked(self, widget):
+        self.connect_handler = self.connect_btn.connect(
+            "clicked", self.on_connection_settings_ready)
+
+        self.cancel_handler = self.cancel_btn.connect(
+            "clicked", self.on_connection_settings_cancelled)
+
+    def on_connect_menu_clicked(self, widget):
         if not hasattr(self, 'view'):
-            self._build_connection_settings()
+            self.create_connection_settings()
 
         self.populate_ip_addrs()
         self.view.show_all()
 
-    def connect_btn_clicked(self, widget):
+    def on_connection_tab_closed(self, tab_label, notebook, tab_widget):
+        notebook.remove_page(notebook.page_num(tab_widget))
+
+    def on_connection_settings_ready(self, widget):
         username = self.builder.get_object(
             'connection_username').get_buffer().get_text()
+
         keypath = self.builder.get_object('connection_key').get_filename()
         ip_addr = self.builder.get_object('instance_ipaddr')
 
         command = ["ssh", "-o StrictHostKeyChecking=no"]
+
         if username is not None:
-            command.append("-l %s" % username)
+            command.append("-l%s" % username)
 
         if keypath is not None:
-            command.append("-i %s" % keypath)
+            command.append("-i%s" % keypath)
 
         iter_ = ip_addr.get_active_iter()
         model = ip_addr.get_model()
@@ -151,23 +201,31 @@ class InstanceContextMenu(Gtk.Menu):
 
         command.append(ip_addr)
         log.debug('Connecting via ssh to host: %s' % ip_addr)
+
         terminal = Vte.Terminal()
         terminal.fork_command_full(Vte.PtyFlags.DEFAULT,
                                    None, command,
                                    [], GLib.SpawnFlags.SEARCH_PATH, None, None)
-
         notebook = self.builder.get_object("notebook1")
-        notebook.append_page(terminal,
-                             Gtk.Label("Connection: <%s>" % self.instance.id))
-        notebook.show_all()
-        self.cancel_btn_clicked(self)
 
-    def cancel_btn_clicked(self, widget):
+        tab_label = TabLabel("Instance: <%s>" % self.instance.name)
+        tab_label.connect("close-clicked", self.on_connection_tab_closed,
+                          notebook,
+                          terminal)
+
+        notebook.append_page(terminal, tab_label)
+        notebook.show_all()
+
+        self.on_connection_settings_cancelled(self)
+
+    def on_connection_settings_cancelled(self, widget):
         self.view.hide()
         if self.connect_handler:
-            self.connect_btn.disconnect(self.connect_handler)
+            self.connect_btn.disconnect(
+                self.connect_handler)
         if self.cancel_handler:
-            self.cancel_btn.disconnect(self.cancel_handler)
+            self.cancel_btn.disconnect(
+                self.cancel_handler)
 
 
 class ConsoleOutputWindow(object):
@@ -212,6 +270,7 @@ class AppWindowHandlers(object):
     def on_toolbtn_view_console_clicked(self, widget):
         tree = self.view.builder.get_object("tree_instances")
         selection = tree.get_selection()
+
         (model, treeiter) = selection.get_selected()
 
         if treeiter is None:
